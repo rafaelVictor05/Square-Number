60f32456ffc60f77f3382f8dfc88c88426a11704
rafaelVictor05 <rafaelredoval8@gmail.com>
2024-10-24 20:03:04 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#### Usage:
+Run the program on the terminal with your desired number.
+```
+python program.py 10
+```
---
+++ program.py
@@ -0,0 +1,16 @@
+import sys
+
+# Verifica se foi passado um argumento
+if len(sys.argv) != 2:
+    print("Usage: programa.app <number>")
+    sys.exit(1)
+
+# Pega o argumento e converte para inteiro
+try:
+    number = int(sys.argv[1])
+except ValueError:
+    print("Please provide a valid integer.")
+    sys.exit(1)
+
+# Calcula e imprime o quadrado do número
+print(number ** 2)
